d2d0dd26a64a5b34ff52da1effbe4ba048fc7662
Matheus Pavanelli <mspavanelli@gmail.com>
2024-03-15 18:59:55 -0300

    chore: add query to create database

---
+++ database/create.sql
@@ -0,0 +1,13 @@
+drop schema call_rider cascade;
+
+create schema call_rider;
+
+create table call_rider.account (
+	account_id uuid primary key,
+	name text not null,
+	email text not null,
+	cpf text not null,
+	car_plate text null,
+	is_passenger boolean not null default false,
+	is_driver boolean not null default false
+);
