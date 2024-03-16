56c2cf654a44b56afbf8f9ba2f1f47cb2d12e762
Matheus Pavanelli <mspavanelli@gmail.com>
2024-03-15 23:21:01 -0300

    refact(signup): remove code smells and improve legibility

--- src/signup.ts
+++ src/signup.ts
@@ -3,76 +3,35 @@ import pgp from "pg-promise";
 import { validateCpf } from "./validate-cpf";
 
 export async function signup(input: any): Promise<any> {
-  const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+  const connection = pgp()(
+    "postgres://postgres:postgres@localhost:5432/call_rider"
+  );
   try {
     const id = crypto.randomUUID();
-
-    const [acc] = await connection.query(
-      "select * from cccat15.account where email = $1",
+    const [existingAccount] = await connection.query(
+      "select * from call_rider.account where email = $1",
       [input.email]
     );
-    if (!acc) {
-      if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-        if (input.email.match(/^(.+)@(.+)$/)) {
-          if (validateCpf(input.cpf)) {
-            if (input.isDriver) {
-              if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-                await connection.query(
-                  "insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
-                  [
-                    id,
-                    input.name,
-                    input.email,
-                    input.cpf,
-                    input.carPlate,
-                    !!input.isPassenger,
-                    !!input.isDriver,
-                  ]
-                );
-
-                const obj = {
-                  accountId: id,
-                };
-                return obj;
-              } else {
-                // invalid car plate
-                return -5;
-              }
-            } else {
-              await connection.query(
-                "insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
-                [
-                  id,
-                  input.name,
-                  input.email,
-                  input.cpf,
-                  input.carPlate,
-                  !!input.isPassenger,
-                  !!input.isDriver,
-                ]
-              );
-
-              const obj = {
-                accountId: id,
-              };
-              return obj;
-            }
-          } else {
-            // invalid cpf
-            return -1;
-          }
-        } else {
-          // invalid email
-          return -2;
-        }
-      } else {
-        // invalid name
-        return -3;
-      }
-    } else {
-      // already exists
-      return -4;
-    }
+    if (existingAccount) throw new Error("Account already exists");
+    if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/))
+      throw new Error("Invalid name");
+    if (!input.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid e-mail");
+    if (!validateCpf(input.cpf)) throw new Error("Invalid CPF");
+    if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/))
+      throw new Error("Invalid car plate");
+    await connection.query(
+      "insert into call_rider.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
+      [
+        id,
+        input.name,
+        input.email,
+        input.cpf,
+        input.carPlate,
+        Boolean(input.isPassenger),
+        Boolean(input.isDriver),
+      ]
+    );
+    return { accountId: id };
   } finally {
     await connection.$pool.end();
   }
